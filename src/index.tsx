15bf94d5da976706ddca6653163f6c4857a39824
Furkan Erdoğan <furkanerdogan@outlook.com.tr>
2023-01-07 19:43:19 +0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import "./assets/styles/globals.scss";
+import Layout from "./components/Layout";
+
+import "./lang/i18n";
+import router from "./routes/router";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <Provider store={store}>
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  </Provider>
+);
---
+++ src/lang/i18n.tsx
@@ -0,0 +1,23 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// Import all translation files
+import en from "./en/common.json";
+import tr from "./tr/common.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  tr: {
+    translation: tr,
+  },
+};
+
+i18next.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+});
+
+export default i18next;
